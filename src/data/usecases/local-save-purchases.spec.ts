dfbb9c9076d7d7e440f58751481553c89264556a
Maycon Oliveira <mroliveira4@huia.haus>
2022-02-17 23:45:43 -0300

    core: ensure LocalSavePurchases deletes old caches on save

--- src/data/usecases/local-save-purchases.spec.ts
+++ src/data/usecases/local-save-purchases.spec.ts
@@ -1,23 +1,38 @@
-describe('LocalSavePurchases', () => {
-    test('Should not delete cache on sut.init',() => {
-        const cacheStore = new CacheStoreSpy()
-        const sut = new LocalSavePurchases(cacheStore)
-
-        expect(cacheStore.deleteCallsCount).toBe(0)
-    })
-})
-
 class LocalSavePurchases {
     constructor(private readonly cacheStore: ICacheStore) {
+    }
+
+    async save (): Promise<void> {
+        this.cacheStore.delete()
     }
 }
 
 interface ICacheStore {
-
+    delete() :void
 }
 
 class CacheStoreSpy implements ICacheStore {
-
     deleteCallsCount = 0
 
+    delete() {
+        this.deleteCallsCount++
+    }
 }
+
+describe('LocalSavePurchases', () => {
+    test('Should not delete cache on sut.init',() => {
+        const cacheStore = new CacheStoreSpy()
+        new LocalSavePurchases(cacheStore)
+
+        expect(cacheStore.deleteCallsCount).toBe(0)
+    })
+
+    test('Should delete old cache  on sut.save',async () => {
+        const cacheStore = new CacheStoreSpy()
+        const sut = new LocalSavePurchases(cacheStore)
+
+        await sut.save()
+        expect(cacheStore.deleteCallsCount).toBe(1)
+    })
+})
+
