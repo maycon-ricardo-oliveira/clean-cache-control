1f085031202489f823658d5d4954a8b2a2ce21c7
Maycon Oliveira <mroliveira4@huia.haus>
2022-02-24 23:53:02 -0300

    core: change asserts to use array to ensure order and quantity count calls

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -2,30 +2,40 @@ import {ICacheStore} from "../protocols/cache/cache-store";
 import {SavePurchases} from "../../domain/usecases/save-purchases";
 
 export class CacheStoreSpy implements ICacheStore {
-    deleteCallsCount = 0
-    insertCallsCount = 0
+    messages: Array<CacheStoreSpy.Message> = []
     deleteKey: string
     insertKey: string
     insertValues: Array<SavePurchases.Params> = []
 
     delete(key): void {
+        this.messages.push(CacheStoreSpy.Message.delete)
         this.deleteKey = key
-        this.deleteCallsCount++
     }
 
     insert(key, value: any): void {
-        this.insertKey = key
-        this.insertCallsCount++
+        this.messages.push(CacheStoreSpy.Message.insert)
         this.insertValues = value
+        this.insertKey = key
     }
 
     simulateDeleteError(): void {
-        jest.spyOn(CacheStoreSpy.prototype, 'delete')
-            .mockImplementationOnce(() => { throw new Error() })
+        jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => {
+            this.messages.push(CacheStoreSpy.Message.delete)
+            throw new Error()
+        })
     }
 
     simulateInsertError(): void {
-        jest.spyOn(CacheStoreSpy.prototype, 'insert')
-            .mockImplementationOnce(() => { throw new Error() })
+        jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
+            this.messages.push(CacheStoreSpy.Message.insert)
+            throw new Error()
+        })
+    }
+}
+
+export namespace CacheStoreSpy {
+    export enum Message {
+        delete,
+        insert
     }
 }
