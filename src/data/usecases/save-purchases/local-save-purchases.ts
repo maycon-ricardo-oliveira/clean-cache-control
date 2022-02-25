2d8185528326d9911a9ee03ad18c8124d72d34df
Maycon Oliveira <mroliveira4@huia.haus>
2022-02-25 00:12:57 -0300

    core: ensure LocalSavePurchases create cache with correct timestamp

--- src/data/usecases/save-purchases/local-save-purchases.ts
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -3,11 +3,17 @@ import {ISavePurchases, SavePurchases} from "../../../domain/usecases/save-purch
 
 
 export class LocalSavePurchases implements ISavePurchases {
-    constructor(private readonly cacheStore: ICacheStore) {
+    constructor(
+        private readonly cacheStore: ICacheStore,
+        private readonly timestamp: Date
+        ) {
     }
 
     async save (purchases: Array<SavePurchases.Params>): Promise<void> {
         this.cacheStore.delete('purchases')
-        this.cacheStore.insert('purchases', purchases)
+        this.cacheStore.insert('purchases', {
+            timestamp: this.timestamp,
+            value: purchases,
+        })
     }
 }
