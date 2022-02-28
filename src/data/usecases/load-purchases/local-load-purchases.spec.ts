f262ba9d584c753086a1b5cd389a33e5412e532d
Maycon Oliveira <mroliveira4@huia.haus>
2022-02-28 15:14:13 -0300

    core: ensure LocalLoadPurchases returns empty list on load failure

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -22,10 +22,20 @@ describe('LocalLoadPurchases', () => {
         const { cacheStore } = makeSut()
         expect(cacheStore.actions).toEqual([])
     })
+
     test('Should call correct key on load',async () => {
         const { cacheStore, sut } = makeSut()
         await sut.loadAll()
         expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch])
         expect(cacheStore.fetchKey).toBe('purchases')
+    })
+
+    test('Should return empty list if load fails',async () => {
+        const { cacheStore, sut } = makeSut()
+        cacheStore.simulateFetchError()
+        const purchases = await sut.loadAll()
+        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch, CacheStoreSpy.Action.delete])
+        expect(cacheStore.deleteKey).toBe('purchases')
+        expect(purchases).toEqual([])
     })
 })
--- src/data/usecases/load-purchases/local-load-purchases.ts
+++ src/data/usecases/load-purchases/local-load-purchases.ts
@@ -1,8 +1,9 @@
 import {ICacheStore} from "../../protocols/cache/cache-store";
 import {ISavePurchases, SavePurchases} from "../../../domain/usecases/save-purchases";
+import {ILoadPurchases, LoadPurchases} from "../../../domain/usecases/load-purchases";
 
 
-export class LocalLoadPurchases implements ISavePurchases {
+export class LocalLoadPurchases implements ISavePurchases, ILoadPurchases {
     private  readonly key = 'purchases';
     constructor(
         private readonly cacheStore: ICacheStore,
@@ -17,7 +18,13 @@ export class LocalLoadPurchases implements ISavePurchases {
         })
     }
 
-    async loadAll(): Promise<void> {
-        this.cacheStore.fetch(this.key)
+    async loadAll(): Promise<Array<LoadPurchases.Result>> {
+        try {
+            this.cacheStore.fetch(this.key)
+            return []
+        } catch(error) {
+            this.cacheStore.delete(this.key)
+            return []
+        }
     }
 }
